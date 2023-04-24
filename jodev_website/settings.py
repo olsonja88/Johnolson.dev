edf554247c96f8e63b42430fa57b4cd8b70ac790
jaolson88 <jaolson190@gmail.com>
2023-04-24 15:48:45 -0500

    Add template dir to settings.py, add projects to installed apps in settings.py

--- jodev_website/settings.py
+++ jodev_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'jodev_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["jodev_website/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
