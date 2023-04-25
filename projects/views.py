a7f8201629b7fe9ad4e420ae3ac9ff919276850c
jaolson88 <jaolson190@gmail.com>
2023-04-25 14:21:42 -0500

    Create index and detail views in views.py

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    # Retrieve all objects in projects table
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    # Retrieve project with primary key pk
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
