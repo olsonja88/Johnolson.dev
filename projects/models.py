accecba60766e642b27bfa07a47fe9d9ff77febb
jaolson88 <jaolson190@gmail.com>
2023-04-25 12:51:42 -0500

    Fix: Fix typo in technology element of models.py

--- projects/models.py
+++ projects/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    techonology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
