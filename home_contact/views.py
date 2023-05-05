0742dc241ecd5e01c2f036966bcc8f6765dcce62
jaolson88 <jaolson190@gmail.com>
2023-05-05 11:07:43 -0500

    Feat: Migrate home_contact models.  Delete contact.html and contact urls to combine home and contact into 1 page.  Change Navbar to have only Contact button instead of Home and Contact.  Put for loop in home.html to display all AboutParagraph objects.

--- home_contact/views.py
+++ home_contact/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from home_contact.models import AboutParagraph
 
 def home(request):
-    return render(request, 'home.html', {})
-
-def contact(request):
-    return render(request, 'contact.html', {})
+    aps = AboutParagraph.objects.all()
+    context = {
+        'aps': aps
+    }
+    return render(request, 'home.html', context)
