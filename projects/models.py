7d7bb4fd4edad7823b3d1721d80d200fe9e2edb7
jaolson88 <jaolson190@gmail.com>
2023-05-05 09:17:39 -0500

    Feat: Add new GitHub URL field to projects/models.py, make migartions.  Add new space for GitHub URL in project_detail.html template.  Assign GitHub URLs to projects objects in python shell.

--- projects/models.py
+++ projects/models.py
@@ -4,4 +4,5 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    gh_url = models.URLField(null=True)
     image = models.FilePathField(path="/projects/img")
