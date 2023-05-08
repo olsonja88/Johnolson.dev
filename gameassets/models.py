ece7057caa0a0a4e66aea543b3590ed7369582ff
jaolson88 <jaolson190@gmail.com>
2023-05-08 12:42:51 -0500

    Feat: Create gameassets models and migrate.

--- gameassets/models.py
+++ gameassets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Sprite(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.FilePathField(path="/gameassets/img")
+
+class Animation(models.Model):
+    name = models.CharField(max_length=20)
+    gif = models.FilePathField(path="/gameassets/gif")
+
+class Soundtrack(models.Model):
+    name = models.CharField(max_length=20)
+    mp3 = models.FilePathField(path="/gameassets/mp3")
