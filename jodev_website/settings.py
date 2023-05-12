52e410c292c2b58c0fef7ead7c9330700723482f
jaolson88 <olsonja88@gmail.com>
2023-05-12 14:12:18 -0500

    Feat: Create requirements.txt and add a STATIC_ROOT in settings.py.

--- jodev_website/settings.py
+++ jodev_website/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'jodev/website/static/'
 
 # Static file dir for static files shared between all pages
 STATICFILES_DIRS = [
