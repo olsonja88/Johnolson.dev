21ceec3fc3036d5d8112e2d129533551f1a2e2e9
jaolson88 <olsonja88@gmail.com>
2023-05-22 09:09:14 -0500

    Fix: Create STATIC_ROOT in settings.py, collect all static files to the root.

--- jodev_website/settings.py
+++ jodev_website/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static_root/'
 
 # Static file dir for static files shared between all pages
 STATICFILES_DIRS = [
