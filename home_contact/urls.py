fcc9e6d1c11fd41ae9a50c258f41a5b6e8d1ce47
jaolson88 <olsonja88@gmail.com>
2023-05-21 21:19:42 -0500

    Fix: Add .vercel to gitignore.  Change home_contact urls.py to use correct import and views.  Add psycopg2 to requirements.txt for pg connection.

--- home_contact/urls.py
+++ home_contact/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from views import index
 
 urlpatterns = [
-    path("", index),
+    path("", views.index, name="index"),
     path("success/", views.success, name="success")
 ]
--- jodev_website/settings.py
+++ jodev_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'jodev_website.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432'            
     }
 }
 
