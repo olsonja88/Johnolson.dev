5bf912097b7b11ca400331de9028eb2f4d7a32c7
jaolson88 <olsonja88@gmail.com>
2023-05-22 10:41:49 -0500

    Test: Change static_url back to 'static_root'.

--- jodev_website/settings.py
+++ jodev_website/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_root/'
 STATIC_ROOT = 'static_root/'
 
 # Static file dir for static files shared between all pages
