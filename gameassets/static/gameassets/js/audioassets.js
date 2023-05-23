c5f8814aca761ede8660381b254035edec1e0035
jaolson88 <olsonja88@gmail.com>
2023-05-22 20:33:35 -0500

    Fix: Refactor EventTarget in line 9 of audioassets.js to event.target

--- gameassets/static/gameassets/js/audioassets.js
+++ gameassets/static/gameassets/js/audioassets.js
@@ -6,7 +6,7 @@ function onlyPlayOneIn(container) {
     audio_elements = container.getElementsByTagName("audio")
         for (i=0; i < audio_elements.length; i++) {
             audio_element = audio_elements[i];
-            if (audio_element !== EventTarget) {
+            if (audio_element !== event.target) {
                 audio_element.pause();
             }
         }
