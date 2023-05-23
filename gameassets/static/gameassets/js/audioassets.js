5d188e2391b8eb4bd947015905346c25a19a51a5
jaolson88 <olsonja88@gmail.com>
2023-05-22 20:26:08 -0500

    Fix: Refactor title.addEventListener in line 5 to container.addEventListener.

--- gameassets/static/gameassets/js/audioassets.js
+++ gameassets/static/gameassets/js/audioassets.js
@@ -2,7 +2,7 @@
 
 /* Function to only allow one audio asset to be played at a time */
 function onlyPlayOneIn(container) {
-    title.addEventListener("play", function(event) {
+    container.addEventListener("play", function(event) {
     audio_elements = container.getElementsByTagName("audio")
         for (i=0; i < audio_elements.length; i++) {
             audio_element = audio_elements[i];
