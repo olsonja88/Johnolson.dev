fc63964683ef52b5ccd3307ac93ec1330be3bcd3
jaolson88 <olsonja88@gmail.com>
2023-05-24 11:27:44 -0500

    Fix: Add photo field to AboutParagraph model in home_contact/models.py

--- home_contact/models.py
+++ home_contact/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class AboutParagraph(models.Model):
     title = models.CharField(max_length=25)
+    photo = models.FilePathField(path="/home_contact/img")
     content = models.TextField()
