1efa431d5f79c5b2a6b7007f30f613515561fd94
jaolson88 <olsonja88@gmail.com>
2023-05-24 11:45:41 -0500

    Fix: Add 'null=True' to all filepath attributes for home_contact and gameasset models.  Make migrations.

--- gameassets/models.py
+++ gameassets/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Sprite(models.Model):
     name = models.CharField(max_length=20)
-    image = models.FilePathField(path="/gameassets/img")
+    image = models.FilePathField(path="/gameassets/img", null=True)
 
 class Animation(models.Model):
     name = models.CharField(max_length=20)
-    gif = models.FilePathField(path="/gameassets/gif")
+    gif = models.FilePathField(path="/gameassets/gif", null=True)
 
 class Soundtrack(models.Model):
     name = models.CharField(max_length=20)
-    mp3 = models.FilePathField(path="/gameassets/mp3")
+    mp3 = models.FilePathField(path="/gameassets/mp3", null=True)
--- home_contact/models.py
+++ home_contact/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class AboutParagraph(models.Model):
     title = models.CharField(max_length=25)
-    photo = models.FilePathField(path="/home_contact/img")
+    photo = models.FilePathField(path="/home_contact/img", null=True)
     content = models.TextField()
